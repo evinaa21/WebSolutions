0a7c15294d0e9dd17caa275f83864f8eeec86b0b
Evina Tershalla <evina.tershalla@gmail.com>
2025-08-19 11:08:35 +0200

    .

--- websolutions/vite.config.js
+++ websolutions/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
   }
 })
